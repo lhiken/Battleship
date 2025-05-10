cfd2cd1b7cb8025665e0c71f5c0dc5657f6a908f
LucaHe09 <lucahe0601@gmail.com>
2025-05-09 22:14:29 -0700

    start page buttons

---
+++ components/ui/start_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_host_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_join_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass
