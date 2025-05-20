ecfd4224fc3d3e102bbc5b4ad00731df43c93032
Rayming Ye <raymingye93@gmail.com>
2025-05-20 16:23:50 -0700

    boom boom shoot

--- src/main/java/entity/Ship.java
+++ src/main/java/entity/Ship.java
@@ -4,10 +4,7 @@ import godot.annotation.Export;
 import godot.annotation.RegisterClass;
 import godot.annotation.RegisterFunction;
 import godot.annotation.RegisterProperty;
-import godot.api.CSGSphere3D;
-import godot.api.CharacterBody3D;
-import godot.api.Node;
-import godot.api.StandardMaterial3D;
+import godot.api.*;
 import godot.core.Color;
 import godot.core.Vector2;
 import godot.core.Vector3;
@@ -22,109 +19,145 @@ import map.gen.Generator;
 @RegisterClass
 public class Ship extends CharacterBody3D {
 
-    private static final GD gd = GD.INSTANCE;
-    private double velocity;
-    private double rotation;
-    private InputState state;
+	private static final GD gd = GD.INSTANCE;
+	private double velocity;
+	private double rotation;
+	private InputState state;
+	private Weapon cannon;
 
-    private double maxVelocity = 10.0;
+	private double maxVelocity = 10.0;
 
-    @RegisterProperty
-    @Export
-    public InputProvider provider;
+	@RegisterProperty
+	@Export
+	public InputProvider provider;
 
-    // pathfinding debug start
-    // everything below is useless lmao
+	// pathfinding debug start
+	// everything below is useless lmao
 
-    @RegisterProperty
-    @Export
-    public Generator gen;
+	@RegisterProperty
+	@Export
+	public Generator gen;
 
-    private int frameCounter = 0;
+	private int frameCounter = 0;
 
-    @RegisterFunction
-    @Override
-    public void _ready() {
-        setMultiplayerAuthority(Integer.parseInt(getName().toString()));
-    }
+	@RegisterFunction
+	@Override
+	public void _ready() {
+		setMultiplayerAuthority(Integer.parseInt(getName().toString()));
+		instantiateNewCannon();
+	}
 
-    @RegisterFunction
-    @Override
-    public void _process(double delta) {
-        if (!isMultiplayerAuthority()) {
-            return;
-        }
-        // frameCounter++;
-        // if (frameCounter % 60 != 0) {
-        //     return;
-        // }
+	@RegisterFunction
+	@Override
+	public void _process(double delta) {
+		if (!isMultiplayerAuthority()) {
+			return;
+		}
+		// frameCounter++;
+		// if (frameCounter % 60 != 0) {
+		//     return;
+		// }
 
-        // Vector3 currentPosition = new Vector3(
-        //     getPosition().getX(),
-        //     0,
-        //     getPosition().getZ()
-        // );
-        // Vector3 endPosition = new Vector3(0, 0, 0);
-        // ArrayList<Coordinate> path = gen.navigate(currentPosition, endPosition);
-        // clearPathVisualization();
+		// Vector3 currentPosition = new Vector3(
+		//     getPosition().getX(),
+		//     0,
+		//     getPosition().getZ()
+		// );
+		// Vector3 endPosition = new Vector3(0, 0, 0);
+		// ArrayList<Coordinate> path = gen.navigate(currentPosition, endPosition);
+		// clearPathVisualization();
 
-        // for (Coordinate coord : path) {
-        //     CSGSphere3D sphere = new CSGSphere3D();
-        //     sphere.setRadius(0.2f);
+		// for (Coordinate coord : path) {
+		//     CSGSphere3D sphere = new CSGSphere3D();
+		//     sphere.setRadius(0.2f);
 
-        //     Vector3 position = coord.toVec3();
-        //     sphere.setPosition(position);
+		//     Vector3 position = coord.toVec3();
+		//     sphere.setPosition(position);
 
-        //     StandardMaterial3D material = new StandardMaterial3D();
-        //     material.setAlbedo(new Color(0, 1, 0, 0.8f));
-        //     sphere.setMaterial(material);
-        //     getParent().addChild(sphere);
+		//     StandardMaterial3D material = new StandardMaterial3D();
+		//     material.setAlbedo(new Color(0, 1, 0, 0.8f));
+		//     sphere.setMaterial(material);
+		//     getParent().addChild(sphere);
 
-        //     pathVisualization.add(sphere);
-        // }
-    }
+		//     pathVisualization.add(sphere);
+		// }
 
-    private ArrayList<Node> pathVisualization = new ArrayList<>();
+		Vector3 position = this.getGlobalPosition();
+		position.setY(position.getY() + 3);
+		cannon.setPosition(position);
 
-    private void clearPathVisualization() {
-        for (Node node : pathVisualization) {
-            removeChild(node);
-            node.queueFree();
-        }
-        pathVisualization.clear();
-    }
+		if (provider != null) {
+			state = provider.getState();
 
-    // pathfinding debug end
+			if (state.getEmittedAction() != -1) {
+				gd.print("pew pew");
 
-    @RegisterFunction
-    @Override
-    public void _physicsProcess(double delta) {
-        if (isMultiplayerAuthority()) {
-            if (provider == null) return;
+				gd.print(state.getPower());
+				cannon.instantiateNewBullet(this.getRotation(), velocity + state.getPower());
 
-            state = provider.getState();
 
-            double targetVelocity = state.getVelocity() * maxVelocity;
-            double targetRotation = state.getRotation();
+			}
+		}
 
-            velocity = gd.lerp(velocity, targetVelocity, 0.1);
-            rotation = gd.lerpAngle(rotation, targetRotation, 0.1);
+	}
 
-            // Rotate the forward direction by the Y rotation
-            Vector3 direction = new Vector3(
-                Math.sin(rotation),
-                0,
-                Math.cos(rotation)
-            );
+	private void instantiateNewCannon() {
 
-            setVelocity(direction.times(velocity));
-            setRotation(new Vector3(0, rotation, 0));
-        }
-        moveAndSlide();
-    }
+		PackedScene weapon = gd.load("res://components/objects/weapon.tscn");
+		cannon = (Weapon) (weapon.instantiate());
 
-    @RegisterFunction
-    public void setProvider(InputProvider provider) {
-        this.provider = provider;
-    }
+		Vector3 pos = this.getGlobalPosition();
+		pos.setY(pos.getY() + 3);
+		cannon.setPosition(pos);
+		cannon.setRotation(this.getRotation());
+
+		getParent().addChild(cannon);
+
+	}
+
+	private ArrayList<Node> pathVisualization = new ArrayList<>();
+
+	private void clearPathVisualization() {
+		for (Node node : pathVisualization) {
+			removeChild(node);
+			node.queueFree();
+		}
+		pathVisualization.clear();
+	}
+
+
+
+	// pathfinding debug end
+
+	@RegisterFunction
+	@Override
+	public void _physicsProcess(double delta) {
+		if (isMultiplayerAuthority()) {
+			if (provider == null) return;
+
+			state = provider.getState();
+
+			double targetVelocity = state.getVelocity() * maxVelocity;
+			double targetRotation = state.getRotation();
+
+			velocity = gd.lerp(velocity, targetVelocity, 0.1);
+			rotation = gd.lerpAngle(rotation, targetRotation, 0.1);
+
+			// Rotate the forward direction by the Y rotation
+			Vector3 direction = new Vector3(
+				Math.sin(rotation),
+				0,
+				Math.cos(rotation)
+			);
+
+			setVelocity(direction.times(velocity));
+			setRotation(new Vector3(0, rotation, 0));
+		}
+		moveAndSlide();
+	}
+
+	@RegisterFunction
+	public void setProvider(InputProvider provider) {
+		this.provider = provider;
+	}
 }
