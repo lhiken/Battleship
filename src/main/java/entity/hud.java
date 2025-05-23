20a03a4e6be1e841e543d217a53b65367a9f8f45
Chen <lauchenxia@gmail.com>
2025-05-23 10:29:03 -0700

    maybe it builds now

--- src/main/java/entity/hud.java
+++ src/main/java/entity/hud.java
@@ -3,21 +3,21 @@ package entity;
 import godot.annotation.RegisterClass;
 import godot.annotation.RegisterFunction;
 import godot.api.CharacterBody3D;
+import godot.api.Panel;
 
 @RegisterClass
-public class Weapon extends CharacterBody3D {
+public class Hud extends Panel {
 
-	@RegisterFunction
-	@Override
-	public void _ready() {
-		// Initialize logic
-	}
+    @RegisterFunction
+    @Override
+    public void _ready() {
+        // Initialize logic
+    }
 
-	@RegisterFunction
-	@Override
-	public void _process(double delta) {
-		// Per-frame logic
-	}
-
-	// Other functions like getCooldown() etc.
+    @RegisterFunction
+    @Override
+    public void _process(double dqelta) {
+        // Per-frame logic
+    }
+    // Other functions like getCooldown() etc.
 }
