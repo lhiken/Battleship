419e220b2456b81c058bb96b7111340563d08eae
Rayming Ye <raymingye93@gmail.com>
2025-05-25 23:43:32 -0700

    IT WORKS!!!

--- src/main/java/entity/providers/BotProvider.java
+++ src/main/java/entity/providers/BotProvider.java
@@ -23,18 +23,19 @@ public class BotProvider extends InputProvider {
     private final double ROTATION_STEP = 4.0;
     private double rotation;
     private double velocity;
-    private int selectedAction;
-    private boolean emitAction;
-    private double power;
+    //    private int selectedAction;
+//    private boolean emitAction;
     private InputState currentState;
+    private int monkey;
 
-    private double turretPitch;
-    private double turretYaw;
     private boolean enemyWithinRadius = false;
+    private Vector3 targetPos;
+    private Vector3 startPos;
+
+    private ArrayList<Coordinate> path;
 
     private int frameCounter;
 
-    private ArrayList<Coordinate> path;
     private Coordinate currentNode;
 
     private Generator gen;
@@ -50,8 +51,11 @@ public class BotProvider extends InputProvider {
         currentState = new InputState();
         rotation = 0;
         velocity = 0;
-        //        selectedAction = 0;
-        //        emitAction = false;
+        targetPos = new Vector3(0, 0, 60);
+        startPos = this.getGlobalPosition();
+        path = gen.navigate(startPos, targetPos);
+//        selectedAction = 0;
+//        emitAction = false;
 
         frameCounter = 0;
     }
@@ -59,46 +63,61 @@ public class BotProvider extends InputProvider {
     @RegisterFunction
     @Override
     public void _process(double delta) {
-        Ship target = (Ship) getParent().getParent().getNode("1");
-        Ship thisShip = (Ship) getParent();
 
-        setAimDirection(
-            target
-                .getGlobalPosition()
-                .plus(
-                    new Vector3(
-                        0,
-                        target
-                                .getGlobalPosition()
-                                .distanceTo(thisShip.getGlobalPosition()) /
-                            30.0 -
-                        1,
-                        0
-                    )
-                ),
-            thisShip.getGlobalPosition(),
-            target.velocityProperty(),
-            thisShip.velocityProperty(),
-            25.0
-        );
+        if (!enemyWithinRadius) {
 
-        emitAction = true;
-        selectedAction = 1;
+            wander();
 
-        // if (!enemyWithinRadius) {
-        //     wander();
-        // }
+        }
+
+        moveToPoint();
 
         updateState();
+
+    }
+
+    public void wander() {
+
+        if (this.getGlobalPosition().isEqualApprox(targetPos)) {
+            do {
+                double randomAngle = Math.random() * 360;
+                double distance = Math.random() * 65;
+                targetPos = new Vector3(Math.cos(Math.toRadians(randomAngle)) * distance, 0, Math.sin(Math.toRadians(randomAngle)) * distance);
+                gd.print("Target position:" + targetPos);
+            } while (!targetPos.equals(new Vector3(0, 0, 0))); // random coordinate generated is on the island
+
+            startPos = this.getGlobalPosition();
+            gd.print("Start Position:" + startPos);
+            path = gen.navigate(startPos, targetPos);
+
+        }
+
+    }
+
+    public void chase() {
+
+    }
+
+    public void moveToPoint() {
+
+        Coordinate temp = path.get(0);
+        Vector3 target = temp.toVec3();
+
+        Vector3 difference = target.minus(this.getGlobalPosition());
+
+        if (difference.isZeroApprox()) {
+            path.remove(0);
+        }
+
+
+        rotation = this.getGlobalPosition().angleTo(target);  // this is definitely not right
+        velocity = 1; // this might be right
+
     }
 
     private void updateState() {
         currentState.velocity = velocity;
         currentState.rotation = rotation;
-        currentState.emitAction = emitAction ? selectedAction : -1;
-        currentState.power = Math.min(14, power * 3);
-        currentState.turretYaw = turretYaw;
-        currentState.turretPitch = turretPitch;
     }
 
     @RegisterFunction
@@ -107,24 +126,6 @@ public class BotProvider extends InputProvider {
         return currentState;
     }
 
-    public void wander() {
-        double randomAngle = Math.random() * 360;
-        double distance = Math.random() * 65;
-        Vector3 randomCoordinate = new Vector3(
-            Math.cos(Math.toRadians(randomAngle)) * distance,
-            0,
-            Math.sin(Math.toRadians(randomAngle)) * distance
-        );
-        if (true) { // random coordinate is not on the island
-            moveToPoint(this.getGlobalPosition(), randomCoordinate);
-        }
-    }
-
-    public void chase() {}
-
-    public void moveToPoint(Vector3 start, Vector3 end) {
-        ArrayList<Coordinate> path = gen.navigate(start, end);
-    }
 
     private void adjustCurrentNode() {}
 
@@ -132,97 +133,4 @@ public class BotProvider extends InputProvider {
         return new Vector3();
     }
 
-    private void setAimDirection(
-        Vector3 P_t,
-        Vector3 P_s,
-        Vector3 V_t,
-        Vector3 V_s,
-        double S_p
-    ) {
-        double t = 2.0;
-
-        // newtons method with like 5 iterations
-        for (int i = 0; i < 5; i++) {
-            double f = f_t(P_t, P_s, V_t, V_s, S_p, t);
-            double f_p = f_pt(P_t, P_s, V_t, V_s, S_p, t);
-
-            if (Math.abs(f_p) < 1e-8) break;
-
-            t = t - f / f_p;
-        }
-
-        // get direction based on solved t
-        Vector3 direction = getProjectileVelocity(
-            P_t,
-            P_s,
-            V_t,
-            V_s,
-            t
-        ).normalized();
-
-        // convert direction into yaw and pitch input
-        turretPitch = Math.asin(direction.getY() / direction.length());
-        turretYaw = Math.atan2(direction.getX(), direction.getZ());
-    }
-
-    private Vector3 getProjectileVelocity(
-        Vector3 P_t,
-        Vector3 P_s,
-        Vector3 V_t,
-        Vector3 V_s,
-        double t
-    ) {
-        Vector3 g = new Vector3(0, -9.8, 0);
-        return P_t.plus(V_t.times(t))
-            .minus(P_s.plus(V_s.times(t)))
-            .minus(g.times(t * t * 0.5))
-            .div(t);
-    }
-
-    // f(t) vector
-    private double f_t(
-        Vector3 P_t,
-        Vector3 P_s,
-        Vector3 V_t,
-        Vector3 V_s,
-        double S_p,
-        double t
-    ) {
-        return (u_t(P_t, P_s, V_t, V_s, S_p, t).length() - S_p * t);
-    }
-
-    // f'(t) vector
-    private double f_pt(
-        Vector3 P_t,
-        Vector3 P_s,
-        Vector3 V_t,
-        Vector3 V_s,
-        double S_p,
-        double t
-    ) {
-        Vector3 u_t = u_t(P_t, P_s, V_t, V_s, S_p, t);
-        Vector3 u_pt = u_pt(V_t, V_s, t);
-        Vector3 g = new Vector3(0, -9.8, 0);
-        return u_t.dot(u_pt) / u_t.length() - S_p;
-    }
-
-    // u(t) vector
-    private Vector3 u_t(
-        Vector3 P_t,
-        Vector3 P_s,
-        Vector3 V_t,
-        Vector3 V_s,
-        double S_p,
-        double t
-    ) {
-        Vector3 R = P_t.minus(P_s); // relative distance
-        Vector3 g = new Vector3(0, -9.8, 0);
-        return (R.plus(V_t.minus(V_s).times(t)).minus(g.times(t * t * 0.5)));
-    }
-
-    // u'(t) vector
-    private Vector3 u_pt(Vector3 V_t, Vector3 V_s, double t) {
-        Vector3 g = new Vector3(0, -9.8, 0);
-        return (V_t.minus(V_s)).minus(g.times(t));
-    }
 }
