bfcce334a69b2888c3dcd49127249f54ef3f2a67
Rayming Ye <raymingye93@gmail.com>
2025-05-28 17:53:42 -0700

    Javadocs for InputState, Ship, and InputProvider

--- src/main/java/entity/InputProvider.java
+++ src/main/java/entity/InputProvider.java
@@ -5,14 +5,24 @@ import godot.annotation.RegisterFunction;
 import godot.api.Node3D;
 import godot.global.GD;
 
+/**
+ * InputProvider is a parent class for it's children classes
+ * that include BotProvider (for bots) and PlayerProvider (for human players)
+ * <p>
+ * InputProvider feeds information about the ship to determine the actions and orientation of the ship
+ */
 @RegisterClass
 public class InputProvider extends Node3D {
 
 	private static final GD gd = GD.INSTANCE;
 
+	/**
+	 * Getter method that returns the inputState of a ship
+	 * @return the inputState
+	 */
 	@RegisterFunction
 	public InputState getState() {
-		gd.print("not implemented grrr");
+		gd.print("not implemented grrr"); // :shocked_emoji:
 		return new InputState();
 	}
 }
--- src/main/java/entity/Ship.java
+++ src/main/java/entity/Ship.java
@@ -17,45 +17,65 @@ import main.GameCameraFrame;
 import main.MatchManager;
 import map.gen.Generator;
 
-/** Ship
- * base ship class
+/**
+ * Base ship class!!!!
+ * <p>
+ *     The character that the player (and bots) will be controlling in order to compete in a match
+ * </p>
+ * <p>
+ *     Ship class operates as a dummy class.
+ *     This class does not actually calculate any values or changes based on environmental or input factors
+ *     The ship class simply takes in values from its inputState and inputProvider in order to determine
+ *     its actions and orientation in the world
+ * </p>
+ * <p>
+ *     The ship mainly acts through its process and physicsProcess methods
+ *     In each method, the ship accesses its inputState in order to determine movement and actions
+ *     The rest of the methods include getter and setter methods to access the ship's private fields
+ * </p>
  */
 @RegisterClass
 public class Ship extends CharacterBody3D {
 
-    private static final GD gd = GD.INSTANCE;
-    private double velocity;
-    private double rotation;
-    private double turretYaw;
-    private double turretPitch;
-    private InputState state;
+	private static final GD gd = GD.INSTANCE;
+	private double velocity;
+	private double rotation;
+	private double turretYaw;
+	private double turretPitch;
+	private InputState state;
 
-    public AudioStreamPlayer boom;
-    public AudioStreamPlayer emptyCannon;
+	private AudioStreamPlayer boom;
+	private AudioStreamPlayer emptyCannon;
 
-    private double maxVelocity = 5.0;
+	private double maxVelocity = 5.0;
 
-    @RegisterProperty
-    @Export
-    public InputProvider provider;
+	/**
+	 * The respective input provider of each ship
+	 */
+	@RegisterProperty
+	@Export
+	public InputProvider provider;
 
-    // pathfinding debug start
-    // everything below is useless lmao
+	// pathfinding debug start
+	// everything below is useless lmao
 
-    @RegisterProperty
-    @Export
-    public Generator gen;
+	/**
+	 * The respective generator of each ship
+	 */
+	@RegisterProperty
+	@Export
+	public Generator gen;
 
-    private int frameCounter = 0;
+	private int frameCounter = 0;
 
-    private double cooldownTime = 2;
-    private double cooldownPercent = 1;
+	private double cooldownTime = 2;
+	private double cooldownPercent = 1;
 
-    private double health = 100;
+	private double health = 100;
 
-    // private MeshInstance3D trajectoryMesh;
+	// private MeshInstance3D trajectoryMesh;
 
-    private boolean sinking = false;
+	private boolean sinking = false;
 
 	/**
 	 * Sets up multiplayer id and gives ship full health when program runs
@@ -72,175 +92,216 @@ public class Ship extends CharacterBody3D {
 	/**
 	 * Runs every single frame and provided by Godot
 	 *
-	 * Uses inputState and inputProvider in order to calculate each ships respective velocity, rotation, shooting, etc
+	 * Uses inputState and inputProvider in order to calculate each ships respective velocity, turret rotation, shooting, etc
 	 * in order to calculate its current orientation and if its shooting
 	 *
-	 * @param delta
+	 * @param delta is the time passed from the last process being called
 	 */
 	@RegisterFunction
 	@Override
 	public void _process(double delta) {
 		frameCounter++;
 
-        cooldownPercent += delta / cooldownTime;
-        cooldownPercent = gd.clamp(cooldownPercent, 0, 1);
+		cooldownPercent += delta / cooldownTime;
+		cooldownPercent = gd.clamp(cooldownPercent, 0, 1);
 
-        if (health <= 0 && !sinking) {
-            getNode("ShipMesh/Sails").queueFree();
-            sinking = true;
-        }
+		if (health <= 0 && !sinking) {
+			getNode("ShipMesh/Sails").queueFree();
+			sinking = true;
+		}
 
-        if (sinking) {
-            globalTranslate(
-                new Vector3(
-                    0,
-                    (getGlobalPosition().getY() * 0.1 - 0.5) * delta * 0.25,
-                    0
-                )
-            );
+		if (sinking) {
+			globalTranslate(
+				new Vector3(
+					0,
+					(getGlobalPosition().getY() * 0.1 - 0.5) * delta * 0.25,
+					0
+				)
+			);
 
-            velocity = gd.lerp(velocity, 0, 0.02);
+			velocity = gd.lerp(velocity, 0, 0.02);
 
-            if (getGlobalPosition().getY() < -15) {
-                queueFree();
-            }
+			if (getGlobalPosition().getY() < -15) {
+				queueFree();
+			}
 
-            sinking = true;
+			sinking = true;
 
-            return;
-        }
+			return;
+		}
 
-        turretYaw = gd.lerpAngle(turretYaw, state.getYaw(), 0.1);
-        turretPitch = gd.lerpAngle(turretPitch, state.getPitch(), 0.1);
+		turretYaw = gd.lerpAngle(turretYaw, state.getYaw(), 0.1);
+		turretPitch = gd.lerpAngle(turretPitch, state.getPitch(), 0.1);
 
-        Vector3 position = this.getGlobalPosition();
-        position.setY(position.getY() + 3);
+		Vector3 position = this.getGlobalPosition();
+		position.setY(position.getY() + 3);
 
-        if (provider != null) {
-            state = provider.getState();
+		if (provider != null) {
+			state = provider.getState();
 
-            if (state.getEmittedAction() != -1 && cooldownPercent == 1) {
-                MatchManager matchManager = (MatchManager) getParent()
-                    .getParent();
-                Vector3 origin =
-                    ((Node3D) getNode(
-                            "Turret/Cannon/ProjectileOrigin"
-                        )).getGlobalPosition();
+			if (state.getEmittedAction() != -1 && cooldownPercent == 1) {
+				MatchManager matchManager = (MatchManager) getParent()
+					.getParent();
+				Vector3 origin =
+					((Node3D) getNode(
+							"Turret/Cannon/ProjectileOrigin"
+						)).getGlobalPosition();
 
-                matchManager.rpc(
-                    StringNames.toGodotName("spawnBullet"),
-                    getMultiplayer().getUniqueId(),
-                    new Vector3(
-                        Math.sin(state.getYaw()),
-                        Math.sin(state.getPitch()),
-                        Math.cos(state.getYaw())
-                    ).normalized(),
-                    origin,
-                    getVelocity()
-                );
-                // gd.print(state.getPower());
-                // cannon.instantiateNewBullet(
-                //     this.getRotation(),
-                //     velocity + state.getPower()
-                // );
+				matchManager.rpc(
+					StringNames.toGodotName("spawnBullet"),
+					getMultiplayer().getUniqueId(),
+					new Vector3(
+						Math.sin(state.getYaw()),
+						Math.sin(state.getPitch()),
+						Math.cos(state.getYaw())
+					).normalized(),
+					origin,
+					getVelocity()
+				);
+				// gd.print(state.getPower());
+				// cannon.instantiateNewBullet(
+				//     this.getRotation(),
+				//     velocity + state.getPower()
+				// );
 
-                // uncommenting this before fixing it will cause process to terminate early
-                // resulting in cooldown never being reset and firing forever!!
-                cooldownPercent = 0;
-                boom = (AudioStreamPlayer) getNode("CannonFire");
-                boom.play();
-            } else if (state.getEmittedAction() != -1 && cooldownPercent < 1) {
-                emptyCannon = (AudioStreamPlayer) getNode("EmptyCannon");
-                emptyCannon.play();
-            }
-        }
-        // drawProjectilePath();
-    }
+				// uncommenting this before fixing it will cause process to terminate early
+				// resulting in cooldown never being reset and firing forever!!
+				cooldownPercent = 0;
+				boom = (AudioStreamPlayer) getNode("CannonFire");
+				boom.play();
+			} else if (state.getEmittedAction() != -1 && cooldownPercent < 1) {
+				emptyCannon = (AudioStreamPlayer) getNode("EmptyCannon");
+				emptyCannon.play();
+			}
+		}
+		// drawProjectilePath();
+	}
 
-    @Rpc(rpcMode = RpcMode.AUTHORITY, sync = Sync.NO_SYNC)
-    @RegisterFunction
-    public void setHealth(double health) {
-        this.health = health;
-        rpc(StringNames.toGodotName("setHealth"), health);
-    }
+	/**
+	 * Setter method that sets health of ship
+	 * @param health health of the ship
+	 */
+	@Rpc(rpcMode = RpcMode.AUTHORITY, sync = Sync.NO_SYNC)
+	@RegisterFunction
+	public void setHealth(double health) {
+		this.health = health;
+		rpc(StringNames.toGodotName("setHealth"), health);
+	}
 
-    @RegisterFunction
-    public double getHealth() {
-        return health;
-    }
+	/**
+	 * Getter method that gets the health of ship
+	 * @return health of ship
+	 */
+	@RegisterFunction
+	public double getHealth() {
+		return health;
+	}
 
-    @RegisterFunction
-    public double getPitch() {
-        return turretPitch;
-    }
+	/**
+	 * Getter method for ship's pitch
+	 * @return the rotation of the ship's turret around the x-axis
+	 */
+	@RegisterFunction
+	public double getPitch() {
+		return turretPitch;
+	}
 
-    @RegisterFunction
-    public double getYaw() {
-        return turretYaw;
-    }
+	/**
+	 * Getter method for the ship's yaw
+	 * @return the rotation of the ship's turret around the y-axis
+	 */
+	@RegisterFunction
+	public double getYaw() {
+		return turretYaw;
+	}
 
-    public InputState getState() {
-        return provider.getState();
-    }
+	/**
+	 * Getter method for inputState
+	 * @return the input state of the ship
+	 */
+	public InputState getState() {
+		return provider.getState();
+	}
 
-    @RegisterFunction
-    @Override
-    public void _physicsProcess(double delta) {
-        if (sinking) {
-            // still apply forward movement (but fading out)
-            Vector3 direction = new Vector3(
-                Math.sin(rotation),
-                0,
-                Math.cos(rotation)
-            );
-            setVelocity(direction.times(velocity));
-            moveAndSlide();
-            return;
-        }
+	/**
+	 * Overriding Godot's built-in function
+	 *
+	 * "processes" differently than Godot's _process method,
+	 * more accurate for physics
+	 *
+	 * handles movement, orientation, sinking and rocking motions based on ship's inputState and inputProvider
+	 * @param delta the time passed between each call to physicsProcess
+	 */
+	@RegisterFunction
+	@Override
+	public void _physicsProcess(double delta) {
+		if (sinking) {
+			// still apply forward movement (but fading out)
+			Vector3 direction = new Vector3(
+				Math.sin(rotation),
+				0,
+				Math.cos(rotation)
+			);
+			setVelocity(direction.times(velocity));
+			moveAndSlide();
+			return;
+		}
 
-        if (isMultiplayerAuthority() && !sinking) {
-            if (provider == null) return;
+		if (isMultiplayerAuthority() && !sinking) {
+			if (provider == null) return;
 
-            state = provider.getState();
+			state = provider.getState();
 
-            double targetVelocity = state.getVelocity() * maxVelocity;
-            double targetRotation = state.getRotation();
+			double targetVelocity = state.getVelocity() * maxVelocity;
+			double targetRotation = state.getRotation();
 
-            velocity = gd.lerp(velocity, targetVelocity, 0.1);
-            rotation = gd.lerpAngle(rotation, targetRotation, 0.1);
+			velocity = gd.lerp(velocity, targetVelocity, 0.1);
+			rotation = gd.lerpAngle(rotation, targetRotation, 0.1);
 
-            Vector3 direction = new Vector3(
-                Math.sin(rotation),
-                0,
-                Math.cos(rotation)
-            );
+			Vector3 direction = new Vector3(
+				Math.sin(rotation),
+				0,
+				Math.cos(rotation)
+			);
 
-            double time = frameCounter / 40.0;
-            double rockX = Math.sin(time * 0.7) * 0.025;
-            double rockZ = Math.sin(time * 0.9) * 0.015;
-            Vector3 rockingOffset = new Vector3(rockX, 0, rockZ);
+			double time = frameCounter / 40.0;
+			double rockX = Math.sin(time * 0.7) * 0.025;
+			double rockZ = Math.sin(time * 0.9) * 0.015;
+			Vector3 rockingOffset = new Vector3(rockX, 0, rockZ);
 
-            setPosition(
-                new Vector3(getPosition().getX(), 0, getPosition().getZ())
-            );
-            setVelocity(direction.times(velocity));
-            setRotation(new Vector3(0, rotation, 0).plus(rockingOffset));
-        }
-        moveAndSlide();
-    }
+			setPosition(
+				new Vector3(getPosition().getX(), 0, getPosition().getZ())
+			);
+			setVelocity(direction.times(velocity));
+			setRotation(new Vector3(0, rotation, 0).plus(rockingOffset));
+		}
+		moveAndSlide();
+	}
 
-    @RegisterFunction
-    public void setProvider(InputProvider provider) {
-        this.provider = provider;
-    }
+	/**
+	 * Setter method for the ship's InputProvider
+	 * @param provider the provider to be set to this ship's provider field
+	 */
+	@RegisterFunction
+	public void setProvider(InputProvider provider) {
+		this.provider = provider;
+	}
 
-    @RegisterFunction
-    public double getCooldownPercentage() {
-        return cooldownPercent;
-    }
+	/**
+	 * Getter method for the ship's cooldown percentage
+	 * @return the ship's cooldown percentage
+	 */
+	@RegisterFunction
+	public double getCooldownPercentage() {
+		return cooldownPercent;
+	}
 
-    @RegisterFunction
-    public boolean isSinking() {
-        return sinking;
-    }
+	/**
+	 * Getter method for if the ship is sinking
+	 * @return boolean depicting ships sinking state
+	 */
+	@RegisterFunction
+	public boolean isSinking() {
+		return sinking;
+	}
 }
