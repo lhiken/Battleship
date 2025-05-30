f310bd5b883432b2a1a0a8643f3028f058b9e513
LucaHe09 <lucahe0601@gmail.com>
2025-05-30 16:30:00 -0700

    javadocs

--- src/main/java/ui/Hud.java
+++ src/main/java/ui/Hud.java
@@ -5,14 +5,24 @@ import godot.annotation.RegisterFunction;
 import godot.api.CanvasLayer;
 import godot.api.Panel;
 
+/**
+ * Heads-up display that player sees
+ */
 @RegisterClass
 public class Hud extends CanvasLayer {
+
+	/**
+	 * Does nothing
+	 */
 	@RegisterFunction
 	@Override
 	public void _ready() {
-		//this.setVisible(false);
 	}
 
+	/**
+	 * Does nothing
+	 * @param delta is time passed since last time process is called
+	 */
 	@RegisterFunction
 	@Override
 	public void _process(double delta) {
--- src/main/java/ui/Settings.java
+++ src/main/java/ui/Settings.java
@@ -12,16 +12,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import multiplayer.MultiplayerManager;
 
+/**
+ * The settings menu that is opened when pressed on start screen
+ */
 @RegisterClass
 public class Settings extends Control {
 
 	private GD gd = GD.INSTANCE;
 	private Control Options;
 
+	/**
+	 * Manages the multiplayer
+	 */
 	@RegisterProperty
 	@Export
 	public MultiplayerManager multiplayerManager;
 
+	/**
+	 * When game starts, settings is not visible
+	 */
 	@RegisterFunction
 	@Override
 	public void _ready() {
@@ -29,12 +38,19 @@ public class Settings extends Control {
 		Options.setVisible(false);
 	}
 
+	/**
+	 * When settings button is clicked, the menu becomes visible
+	 */
 	@RegisterFunction
 	public void _onSettingsClicked() {
 		Options.setVisible(true);
 		gd.print("yay clicked");
 	}
-	
+
+	/**
+	 * When the return button on the settings
+	 * menu is clicked, make settings invisible
+	 */
 	@RegisterFunction
 	public void _onReturnClicked() {
 		if (Options.isVisible()
