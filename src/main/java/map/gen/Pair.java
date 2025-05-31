dca487421412f7542b495a859df19437e102ab20
LucaHe09 <lucahe0601@gmail.com>
2025-05-30 20:01:51 -0700

    morre javadoks

--- src/main/java/map/gen/Pair.java
+++ src/main/java/map/gen/Pair.java
@@ -19,23 +19,34 @@ public class Pair<K, V> {
     private final V second;
 
     /**
-     *
-     * @param first
-     * @param second
+     * Sets current first and second to parameters first and second
+     * @param first is parameter to replace current first
+     * @param second is parameter to replace current second
      */
     public Pair(K first, V second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * @return first
+     */
     public K getFirst() {
         return first;
     }
 
+    /**
+     * @return second
+     */
     public V getSecond() {
         return second;
     }
 
+    /**
+     * Checks if current hashmap pair is equal to o's
+     * @param o is object to compare with
+     * @return true or false of statement
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -45,11 +56,17 @@ public class Pair<K, V> {
         return (first == pair.first && second == pair.second);
     }
 
+    /**
+     * @return hashcode of first and second
+     */
     @Override
     public int hashCode() {
         return Objects.hash(first, second);
     }
 
+    /**
+     * @return string of first and second
+     */
     @Override
     public String toString() {
         return "Pair{" + "first=" + first + ", second=" + second + '}';
