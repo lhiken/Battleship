b061862b485f5ae3871b2998edb05cb3063b4776
Rayming Ye <raymingye93@gmail.com>
2025-05-30 19:15:50 -0700

    SeaMINES

--- src/main/java/entity/Seamine.java
+++ src/main/java/entity/Seamine.java
@@ -9,7 +9,7 @@ import main.GameCamera;
 import multiplayer.MultiplayerManager;
 
 @RegisterClass
-public class Seamine extends MeshInstance3D {
+public class Seamine extends Node3D {
 
     private GD gd = GD.INSTANCE;
 
@@ -17,37 +17,51 @@ public class Seamine extends MeshInstance3D {
     private int ownerId = 1;
     private Area3D area;
     private CollisionShape3D collision;
+    private boolean justExploded;
 
-    @RegisterProperty
-    @Export
-    public PackedScene explosion;
+    private double cooldown;
+    double globalPosition;
+
+//    @RegisterProperty
+//    @Export
 
     @RegisterFunction
     @Override
     public void _ready() {
         area = (Area3D) getNode("Area3D");
+        cooldown = 0;
         collision = (CollisionShape3D) getNode("Area3D").getNode("CollisionShape3D");
+        justExploded = true;
+        globalPosition = -1;
     }
 
     @RegisterFunction
     @Override
     public void _process(double delta) {
-
+        if (justExploded) {
+            if (getGlobalPosition().getY() > -10) {
+                translate(new Vector3(0, -1 * delta, 0));
+            }
+            cooldown += delta;
+        }
+        if (cooldown > 20) {
+            justExploded = false;
+            if (getGlobalPosition().getY() < -1) {
+                translate(new Vector3(0, delta, 0));
+            }
+        }
     }
 
     @RegisterFunction
-    public void bodyEntered(Node body) {
-        spawnExplosion();
-    }
-
-    @RegisterFunction
-    public void onCollision(Node body) {
-        if (!getMultiplayer().isServer()) return;
-        collisionBody = body;
-        spawnExplosion();
-        if (body instanceof Ship) {
-            MultiplayerManager manager = MultiplayerManager.Instance;
-            manager.invokeBulletDamage(ownerId, (Ship) body, 30);
+    public void bodyEntered(Node3D body) {
+        if (cooldown > 20) {
+            spawnExplosion();
+            if (body instanceof Ship) {
+                MultiplayerManager manager = MultiplayerManager.Instance;
+                manager.invokeBulletDamage(ownerId, (Ship) body, 30);
+            }
+            cooldown = 0;
+            justExploded = true;
         }
     }
 
@@ -57,27 +71,33 @@ public class Seamine extends MeshInstance3D {
     @Rpc(rpcMode = RpcMode.AUTHORITY, sync = Sync.NO_SYNC)
     @RegisterFunction
     public void spawnExplosion() {
+        gd.print("Explosion spawned");
 
+        PackedScene explosion = (PackedScene) gd.load("res://shaders/particles/Prefab.tscn");
         Node3D explosionNode = (Node3D) explosion.instantiate();
         getParent().addChild(explosionNode);
         explosionNode.setRotation(new Vector3(0, Math.PI/2, 0));
         explosionNode.setGlobalPosition(getGlobalPosition());
-        queueFree();
 
-        GameCamera camera = (GameCamera) getParent()
-                .getParent()
-                .getNode("RenderTarget/Viewport/GameCamera");
-        camera.invokeShake(
-                // inverse square falloff
-                (1 /
-                        Math.pow(
-                                positionProperty().distanceTo(camera.getPosition()),
-                                2
-                        )) *
-                        100
-        );
+
+
+
+
+//        GameCamera camera = (GameCamera) getParent()
+//                .getParent()
+//                .getNode("RenderTarget/Viewport/GameCamera");
+//        camera.invokeShake(
+//                // inverse square falloff
+//                (1 /
+//                        Math.pow(
+//                                positionProperty().distanceTo(camera.getPosition()),
+//                                2
+//                        )) *
+//                        100
+//        );
 
         rpc(StringNames.toGodotName("spawnExplosion"));
+
     }
 
 
